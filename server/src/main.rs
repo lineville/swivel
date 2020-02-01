d8368adf65641688275de1d35bbe5ddac29bb76b
lineville <lineville@vassar.edu>
2020-01-31 16:24:49 -0800

    restructured not breaking...
    Signed-off-by: lineville <lineville@vassar.edu>

---
+++ server/src/main.rs
@@ -0,0 +1,51 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate juniper;
+
+mod model;
+mod schema;
+
+use juniper::{EmptyMutation, RootNode};
+use model::Database;
+use rocket::response::content;
+use rocket::State;
+use schema::Query;
+use rocket_contrib::serve::StaticFiles;
+
+type Schema = RootNode<'static, Query, EmptyMutation<Database>>;
+
+// #[rocket::get("/")]
+// fn graphiql() -> content::Html<String> {
+//     juniper_rocket::graphiql_source("/graphql")
+// }
+
+// #[rocket::get("/graphql?<request>")]
+// fn get_graphql_handler(
+//     context: State<Database>,
+//     request: juniper_rocket::GraphQLRequest,
+//     schema: State<Schema>,
+// ) -> juniper_rocket::GraphQLResponse {
+//     request.execute(&schema, &context)
+// }
+
+// #[rocket::post("/graphql", data = "<request>")]
+// fn post_graphql_handler(
+//     context: State<Database>,
+//     request: juniper_rocket::GraphQLRequest,
+//     schema: State<Schema>,
+// ) -> juniper_rocket::GraphQLResponse {
+//     request.execute(&schema, &context)
+// }
+
+fn main() {
+    rocket::ignite()
+        .manage(Database::new())
+        .manage(Schema::new(Query, EmptyMutation::<Database>::new()))
+        // .mount(
+        //     "/",
+        //     rocket::routes![graphiql, get_graphql_handler, post_graphql_handler],
+        // )
+        .mount("/", StaticFiles::from("/client/build"))
+        .launch();
+}
